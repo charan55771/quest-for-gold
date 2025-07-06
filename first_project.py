078ad40e6792fc8945da402baa6fa3131cffcd06
charan <pillivenkatasaicharanreddy@gmail.com>
2025-07-06 11:00:41 +0530

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -1,29 +1,31 @@
 name = input("Type your name: ")
-print("Hello", name, "welcome to my Game!" )
+print("Hello", name, "welcome to my Game!")
 
-should_we_play = input("Do you want to play? ").lower()
+should_we_play = input("Do you want to play?(yes/no)   ").lower()
 
-while should_we_play == "yes" or should_we_play == "y" :
-    print("Lets Begin! ")
-    weapon = input("choose a weapon (axe/rope) ").lower()
-    direction = input("Which way do you choose,left or right(left/right) :").lower()
-    if direction == "left" :
-        if weapon == "rope" :
-            print("You crossed level 1 succesfully! ")
-        else :
-            print("Sorry you took the wrong step,YOU DIE.. ")
-    elif direction == "right" :
-        choice = input("You crossed level 1 succesfully and now there is a bridge do you wonna cross it or do you want to swin under it?(Cross/swim) ")
-        if choice == "swim" :
-            if weapon == "axe":
-                print("YOU FOUND THE GOLD!! GAME OVER.... ")
-            else :
-                print("You are eaten by an aligator and GAME OVER...  ")
-        else :
-            print("YOU FOUND THE GOLD!! GAME OVER.... ")
-    else :
-        print("Sorry not a valid input,GAME OVER... ")
-    
-    should_we_play = input("Do you want to play again? (yes/no) ").lower()
-else :
+while should_we_play == "yes" or should_we_play == "y":
+    print("Let's Begin!")
+    weapon = input("Choose a weapon (axe/rope): ").lower()
+    direction = input("Which way do you choose, left or right (left/right): ").lower()
+    if direction == "left":
+        if weapon == "rope":
+            print("You crossed level 1 successfully!")
+        else:
+            print("Sorry you took the wrong step, YOU DIE..")
+    elif direction == "right":
+        print("You have crossed level 1 successfully")
+    else:
+        print("Sorry not a valid input, GAME OVER...")
+
+    choice = input("Now there is a bridge. Do you want to cross it or swim under it? (cross/swim): ").lower()
+    if choice == "swim":
+        if weapon == "axe":
+            print("YOU FOUND THE GOLD!! GAME OVER....")
+        else:
+            print("You are eaten by an alligator and GAME OVER...")
+    else:
+        print("YOU FOUND THE GOLD!! GAME OVER....")
+
+    should_we_play = input("Do you want to play again? (yes/no): ").lower()
+else:
     print("END....")
